9dac47e9b0179cdd91948599bb12876dcd194c30
Vishal Singh <vishal.kumar@gotoko.co.id>
2022-10-08 18:06:03 +0530

    Added DB Connection , Added DB Migration for author , book , event table

--- server/server.go
+++ server/server.go
@@ -1,1 +1,21 @@
 package server
+
+import (
+	"fmt"
+
+	"github.com/vishal2911/school/store/pgress"
+)
+
+type Server struct{
+
+
+
+}
+
+func (s Server)NewServer() {
+
+	fmt.Println("Creating new server .....")
+	pgressStore := pgress.PgressStore{}
+	pgressStore.NewStore() 
+
+}
